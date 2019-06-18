f01e07af62da8ffee44e4216a7c413b8219e0433
Shamiul Mowla <mowla@adobe.com>
2019-06-18 15:09:08 -0400

    Scripts to run allure reports and generate results. Travis to push the reports to surge.sh temp

--- karma.conf.js
+++ karma.conf.js
@@ -56,7 +56,7 @@ module.exports = config => {
     reporters: ["spec", "allure"],
 
     allureReport: {
-      reportDir: "test/results/allure-report/", // By default files will be save in the base dir
+      reportDir: "test/allure/allure-results/", // By default files will be save in the base dir
       useBrowserName: true // add browser name to report and classes names
     },
 
